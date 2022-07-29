61b2b3b4da872d6d07e997d572c877a50d3eafe7
Sophie Schorochoff <sophie.schorochoff@gmail.com>
2022-07-29 10:20:12 +0200

    Add Project for tests

---
+++ StringExtensionsBspmLibraryTests/StringExtensionsTimeTests.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringExtensionsBspmLibrary;
+using System;
+
+namespace StringExtensionsBspmLibraryTests
+{
+    [TestClass]
+    public class StringExtensionsTimeTests
+    {
+        [TestMethod]
+        public void TimeSpanExtensions_Time_ToTimeSpan()
+        {
+            Assert.AreEqual(new TimeSpan(0, 0, 12, 34, 567), "12:34.567".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 12, 34, 567), "12:34:567".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 12, 34, 567), "12:34,567".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 12, 34, 567), "12.34.567".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 12, 34, 567), "12\'34\"567".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 12, 34, 567), $"12'34''567".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 1, 2, 3), "1:02.003".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 1, 2, 3), $"1\'02''003".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 1, 2, 345), "1:2.345".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 1, 2, 0), "1:2.0".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 1, 2, 0), $"1\'2''0".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 12, 34, 500), "12:34.5".ToTimeSpan());
+            Assert.AreEqual(new TimeSpan(0, 0, 12, 34, 560), "12\'34\'\'56".ToTimeSpan());
+            Assert.AreEqual(null, "123\'00\'\'00".ToTimeSpan());
+            Assert.AreEqual(null, "32".ToTimeSpan());
+            Assert.AreEqual(null, "".ToTimeSpan());
+            Assert.AreEqual(null, "Azerty34".ToTimeSpan());
+            Assert.AreEqual(null, ((string?)null).ToTimeSpan());
+        }
+    }
+}
