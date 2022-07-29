5fc89335cbae6f2c10ee95a24389e75f26c7fead
Sophie Schorochoff <sophie.schorochoff@gmail.com>
2022-07-29 14:01:12 +0200

    add tests

---
+++ StringExtensionsBspmLibraryTests/TODO.cs
@@ -0,0 +1,26 @@
+﻿namespace StringExtensionsBspmLibraryTests
+{
+    class TODO
+    {
+
+        //[TestMethod]
+        //public void StringExtensions_IsValidEmail_WithValidEmails()
+        //{
+        //    Assert.IsTrue("romain1@oniryx.eu".IsValidEmail());
+        //    Assert.IsTrue("romain.marbaix@oniryx.eu".IsValidEmail());
+        //    Assert.IsTrue("romain.marbaix@s1.support.oniryx.eu".IsValidEmail());
+        //    Assert.IsTrue("support+romainmarbaix@oniryx.eu".IsValidEmail());
+        //}
+
+        //[TestMethod]
+        //public void StringExtensions_IsValidEmail_WithInvalidEmails()
+        //{
+        //    Assert.IsFalse("".IsValidEmail());
+        //    Assert.IsFalse("romainmarbaix".IsValidEmail());
+        //    Assert.IsFalse("@oniryx.eu".IsValidEmail());
+        //    Assert.IsFalse("romain.marbaix@oni@ryx.eu".IsValidEmail());
+        //    Assert.IsFalse("romain.marbaix@oniryxeu".IsValidEmail());
+        //}
+
+    }
+}
